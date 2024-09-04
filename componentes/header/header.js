19b556dc6de6debc963ec957815aa922f1512247
dmosorio <dmosorio@scl.edu.gt>
2024-09-04 11:19:12 -0600

    headerr

---
+++ componentes/header/header.js
@@ -0,0 +1,17 @@
+let header = `
+<header class="coso">
+    <h1>TechMap</h1>
+
+    <nav>
+         <a href="#">SSD</a>
+        <a href="#">RAM</a>
+        <a href="#">HDD</a>
+        <a href="#">GPU</a>
+        <a href="#">CPU</a>
+        <a href="#">Fuentes de poder</a>
+         <a href="#">Placa madre</a>
+    </nav>
+    <a href="https://github.com/ROCKEMMA/techMap-danna.git" class="github-btn">Github</a>
+</header>
+`;
+export{header}
