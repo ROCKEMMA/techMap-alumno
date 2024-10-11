db85a852457645a96dddd233e28fa3a90ab68b2f
dmosorio <dmosorio@scl.edu.gt>
2024-10-10 20:08:36 -0600

    header_nuevo

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 import { header } from "./componentes/header/header.js"
 
-let conexion = document.querySelector('.root');
+let root = document.querySelector(".root")
 
-function cargardiv(){
-    conexion.innerHTML=`
-    <div class="header">${header}</div>
-    `;
-}
-cargardiv();
+root.appendChild(header);
